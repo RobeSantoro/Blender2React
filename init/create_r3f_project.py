bc60d6433e078ccadc9a6ef65ae2edd27c5bff48
RobeSantoro <santoro.robe@gmail.com>
2024-01-31 17:43:29 +0100

    Add macOs in create_r3f_project

--- init/create_r3f_project.py
+++ init/create_r3f_project.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import re
 import subprocess
 import bpy
@@ -6,16 +7,15 @@ import bpy
 from .. B2REACT_Globals import get_scene_path, get_project_root, get_project_name
 
 
-def create_r3f_project(R3F_Project_Root, R3F_Project_Name):
-    """Creates a R3F Project in a folder in the given path"""
-
-    print('-------------------------------------------------------')
-    print('Executing command in path:', R3F_Project_Root, '\n')
+def create_r3f_project(R3F_Project_Root="R3F_Project_Root",
+                       R3F_Project_Name="R3F_Project_Name",
+                       ):
+    """Creates a R3F Project in a folder called as R3F_Project_Name in the given Root path"""
 
     os.chdir(R3F_Project_Root)
-    cmd = f"echo Creating '{R3F_Project_Name}' R3F Project in '{R3F_Project_Root}'...\
-        && echo. \
-        && cd {R3F_Project_Root}\
+
+    cmd = f"\
+        cd {R3F_Project_Root}\
         && git clone https://github.com/RobeSantoro/blender2react-template {R3F_Project_Name}\
         && cd {R3F_Project_Name}\
         && git remote rm origin\
@@ -24,13 +24,34 @@ def create_r3f_project(R3F_Project_Root, R3F_Project_Name):
         && exit\
         "
 
-    p = subprocess.Popen(["start", "cmd", "/k", f"{cmd}"], shell=True)
-    p.wait()
-
+    # Clean cmd for printing
     cmd_clean = re.sub(' {2,}', ' ', cmd)
     cmd_clean = cmd_clean.replace("&&", "&&\n")
     print(cmd_clean)
+
+    # Run cmd in a new terminal
+    platform_system = platform.system()
     print('-------------------------------------------------------')
+    print('----------------Creating R3F Project...----------------')
+    print('-------------------------------------------------------')
+    print()
+    print('R3F_Project_Root:', R3F_Project_Root)
+    print('R3F_Project_Name:', R3F_Project_Name)
+    print()
+    print("platform_system:", platform_system)
+
+    if platform_system == "Windows":
+        p = subprocess.Popen(["start", "cmd", "/k", f"{cmd}"], shell=True)
+        p.wait()
+    elif platform_system == "Darwin":
+        try:
+            output = os.system(cmd)
+            print("output:", output)
+            print(f" Project Created in {R3F_Project_Root}/{R3F_Project_Name}")
+        except Exception as e:
+            print(e)
+    elif platform_system == "Linux":
+        pass
 
     return
 
@@ -54,7 +75,8 @@ class B2REACT_OT_Create_R3F_Project(bpy.types.Operator):
         # Check if the Blender Scene has been saved
         Scene_Path = get_scene_path()
         if Scene_Path is None:
-            self.report({"ERROR"}, "Please save the Blender Scene before creating a R3F Project")
+            self.report(
+                {"ERROR"}, "Please save the Blender Scene before creating a R3F Project")
             return {"CANCELLED"}
 
         # Get R3F Project Root and Name from the UI
@@ -84,6 +106,9 @@ class B2REACT_OT_Create_R3F_Project(bpy.types.Operator):
 
     def draw(self, context):
         row = self.layout
-        row.prop(context.scene.Blender2React, "R3F_Project_Root", text="Project Root")
-        row.prop(context.scene.Blender2React, "R3F_Project_Name", text="Project Name")
-        row.label(text="If no path is given, will create a 'r3f-project' folder next to the blender file")
+        row.prop(context.scene.Blender2React,
+                 "R3F_Project_Root", text="Project Root")
+        row.prop(context.scene.Blender2React,
+                 "R3F_Project_Name", text="Project Name")
+        row.label(
+            text="If no path is given, will create a 'r3f-project' folder next to the blender file")
