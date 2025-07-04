4a24208fa33f0106e410b2ec8dc67277616d69a7
RobeSantoro <santoro.robe@gmail.com>
2025-07-04 02:15:58 +0200

    Fix git reset cmd

--- init/reset_git.py
+++ init/reset_git.py
@@ -3,6 +3,7 @@ import shutil
 import subprocess
 import re
 import bpy
+import platform
 
 from . delete_r3f_project import remove_readonly
 from .. B2REACT_Globals import get_project_root, get_project_name
@@ -31,38 +32,46 @@ class B2REACT_OT_Reset_Git(bpy.types.Operator):
         print('-------------------------------------------------------')
         print('Deleting Actual Git Folder:', git_path)
 
-        # If exists
+        # If exists, delete .git folder
         if os.path.exists(git_path):
-
-            # Delete .git folder
             shutil.rmtree(git_path, onerror=remove_readonly)
             print('.git Folder Deleted')
-        
-            cmd = f"cd {R3F_Project_Root + R3F_Project_Name}\
-                && git init\
-                && git add .\
-                && git commit -m \"New\"\
-                && timeout 15\
-                && exit\
-            "
-
-            p = subprocess.Popen(["start", "cmd", "/k", f"{cmd}"], shell=True)
-            p.wait()
-
-            cmd_clean = re.sub(' {2,}', ' ', cmd)
-            cmd_clean = cmd_clean.replace("&&", "&&\n")
-
-            print('Executing command in path:', R3F_Project_Root + R3F_Project_Name, '\n')
-
-            print("Git Initialized")
-            print('-------------------------------------------------------')
         else:
             print('Git Folder not found')
-            bpy.context.window_manager.popup_menu(
-                lambda self, context:
-                    self.layout.label(text="Git Folder not found"),
-                    title="Git Folder not found",
-                    icon='ERROR')
+
+        # Always initialize git after deleting (or if not found)
+        project_path = os.path.join(R3F_Project_Root, R3F_Project_Name)
+        platform_system = platform.system()
+
+        if platform_system == "Windows":
+            # Windows command
+            cmd = f'cd /d "{project_path}" && git init && git add . && git commit -m "New Initial Commit" && timeout 10 && exit'
+            subprocess.Popen(["start", "cmd", "/k", cmd], shell=True)
+            
+        elif platform_system == "Darwin":
+            # macOS command
+            command = f"cd '{project_path}' && git init && git add . && git commit -m 'New Initial Commit' && sleep 10 && exit"
+            apple_script = f'tell application "Terminal" to do script "{command}"'
+            subprocess.Popen([
+                "osascript",
+                "-e",
+                apple_script
+            ])
+        else:
+            # Linux/Unix command
+            command = f'cd "{project_path}" && git init && git add . && git commit -m "New Initial Commit" && sleep 10'
+            subprocess.Popen(["x-terminal-emulator", "-e", "bash", "-c", command])
+
+        print('Executing git initialization in terminal...')
+        print('Project path:', project_path)
+        print("Git Initialized")
+        print('-------------------------------------------------------')
+
+        bpy.context.window_manager.popup_menu(
+            lambda self, context:
+                self.layout.label(text="Created a new git repo"),
+                title="Git Reset Complete",
+                icon='INFO')
 
         return {"FINISHED"}
     
